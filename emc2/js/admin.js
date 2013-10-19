7b4b7dacd637043b8a56ba4ad04aeef3b59862a0
Math club <exetermathclub@gmail.com>
2013-10-19 12:42:32 -0400

    fixed admin logout bug

--- emc2/js/admin.js
+++ emc2/js/admin.js
@@ -1,16 +1,19 @@
-$(function() {
+$(function () {
+    "use strict";
+
     //Retrieve the locally stored session key and id
     var srp_string = localStorage.getItem("SRP_SESS_KEY"),
         srp_id = localStorage.getItem("SRP_ID"),
         srp_key;
-    
 
     //If we haven't logged in correctly, tell the user to do so
-    if (srp_string == null || srp_id == null) location.href = "login.shtml";
-    
+    if (!srp_string || !srp_id) {
+        location.href = "login.shtml";
+    }
+
     //Parse the locally stored srp key
     srp_key = CryptoJS.enc.Base64.parse(srp_string);
-    
+
     function team_format(row) {
         var table_row = $("<tr>")
             .append($("<td class='team_id'>").text(row[0]))
@@ -20,7 +23,7 @@ $(function() {
             .append($("<td class='team_participation'>").text(row[5] ? "On-Site" : "Online"))
             .append($("<td class='team_payment'>").append((row[4] ? $("<input type='checkbox'/>").attr("checked", "") : $("<input type='checkbox'/>"))
                 .addClass("payment_checkbox")
-                .click(function() {
+                .click(function () {
                     $.ajax({
                         url: "../wsgi-scripts/toggle_payment.py",
                         data: {
@@ -29,7 +32,7 @@ $(function() {
                             "paid": this.checked ? 1 : 0
                         },
                         success: function (data) {
-                            if(!data.success) {
+                            if (!data.success) {
                                 alert("Action failed");
                             }
                         }
@@ -37,15 +40,15 @@ $(function() {
                 })
             ))
             .append($("<td class='team_deletion'>").append($("<button class='field'>").text("Delete")
-                .click(function() {
+                .click(function () {
                     if (confirm("Are you sure you want to delete " + row[2] + "?")) {
                         $.ajax({
                             "url": "../wsgi-scripts/delete_team.py",
                             "data": {
                                 "teamid": JSON.stringify(encrypt(srp_key, row[0].toString())),
-                                "id": srp_id,
+                                "id": srp_id
                             },
-                            success: function() {
+                            success: function () {
                                 table_row.remove();
                             }
                         });
@@ -57,18 +60,24 @@ $(function() {
 
     //Request all the teams
     $.ajax({
-        "url": "../wsgi-scripts/admin_teams.py",
-        "method": "GET",
-        "data": {
-            "id": srp_id
+        url: "../wsgi-scripts/admin_teams.py",
+        method: "GET",
+        data: {
+            id: srp_id
         },
-        "dataType": "json",
-        "success": function (data) {
+        dataType: "json",
+        success: function (data) {
             var decoded = JSON.parse(decrypt(srp_key, data)),
-                team_list_el = $("#team_table");
-            for (var i = 0; i < decoded.length; i += 1) {
+                team_list_el = $("#team_table"),
+                i;
+            for (i = 0; i < decoded.length; i += 1) {
                 team_list_el.append(team_format(decoded[i]));
             }
+        },
+        error: function () {
+            localStorage.removeItem("SRP_SESS_KEY");
+            localStorage.removeItem("SRP_ID");
+            window.location.href = "login.shtml";
         }
     });
 });
